48e011e69b327c11e7af7794e13b408e9653ad1a
Hunter Durbin <hunterdurbin000@gmail.com>
2026-02-07 09:13:45 -0500

    Add helper function for getting opponent columns and destroying zones

--- scripts/effects/esd01/esd01_016.gd
+++ scripts/effects/esd01/esd01_016.gd
@@ -8,22 +8,5 @@ extends CardEffect
 
 func on_enter(ctx: EffectContext) -> void:
 	var monster_zone_idx: int = ctx.owner.monster_zone - 1  # 0-indexed
-	_destroy_opponent_column(ctx, monster_zone_idx)
-
-
-func _destroy_opponent_column(ctx: EffectContext, zone_idx: int) -> void:
-	var opponent := ctx.opponent
-	var zones_to_check: Array[int] = get_opponent_column_zones(zone_idx)
-
-	var destroyed_any := false
-	for zi in zones_to_check:
-		if zi >= 0 and zi < 8 and not opponent.is_zone_empty(zi):
-			var top_card := opponent.get_zone_top_card(zi)
-			var stack: Array = opponent.clear_zone(zi)
-			opponent.discard_pile.append_array(stack)
-			ctx.effect_handler.trigger_revenge(opponent.player_id, top_card)
-			destroyed_any = true
-
-	if destroyed_any:
-		opponent.zones_changed.emit()
-		opponent.discard_changed.emit()
+	var column_zones := get_opponent_column_zones(monster_zone_idx)
+	ctx.effect_handler.destroy_zones(ctx.opponent, column_zones)
