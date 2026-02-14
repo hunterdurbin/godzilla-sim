651ab8e7a57d161525b513d60d2ab1b8e3f4a7d2
Hunte Durbin <hunterdurbin000@gmail.com>
2026-02-13 19:52:02 -0500

    Test ebp01-010

--- scripts/effects/ebp01/ebp01_010.gd
+++ scripts/effects/ebp01/ebp01_010.gd
@@ -6,7 +6,7 @@ extends CardEffect
 ## <Opponent's Turn> At the beginning of the counter phase, if this card has 3 or more <Rage>,
 ## <Destroy> all of your opponent's battle cards in the same column as this card.
 ##
-## Tested: No
+## Tested: Yes
 ## Known issues: None
 ## Edge cases: None
 ## Rules: None
